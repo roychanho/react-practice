a5a9dc077f121a837e6886b80ed2b593d12965da
CHAN Ho Long <roychanho@gmail.com>
2023-01-15 23:57:41 +0800

    update

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+
+// const DUMMY_QUOTES = [
+//   { id: "q1", author: "Max", text: "Learning React is fun!" },
+//   { id: "q2", author: "Maximilian", text: "Learning React is great!" },
+// ];
+
+const AllQuotes = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
+};
+
+export default AllQuotes;
